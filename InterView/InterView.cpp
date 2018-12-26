f807ca63b28957e15c809d48ef90a57f340de9bf
baluoteliz <a18772316113@163.com>
2018-12-26 14:05:51 +0800

    update vxproj

--- InterView/InterView.cpp
+++ InterView/InterView.cpp
@@ -1,11 +1,6 @@
 // InterView.cpp : Defines the entry point for the console application.
 //
 
-/*
-* About: 钉钉:
-* 一堆数据（Key,Value)，需要对这些数据分类，同时保证先进入的数据在前面.
-*/
-
 #include <string>
 #include <iostream>
 using namespace std;
